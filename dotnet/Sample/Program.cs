280156012bae59a88e193ca0b5982df51e682acd
MinjueWu <597922968@qq.com>
2023-07-05 22:34:13 +0800

    Add dotnet sample

---
+++ dotnet/Sample/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Azure.AI.ContentSafety.Dotnet.Sample
+{
+    class Program
+    {
+        static void Main()
+        {
+            ContentSafetySampleAnalyzeText sampleAnalyzeText = new ContentSafetySampleAnalyzeText();
+            sampleAnalyzeText.AnalyzeText();
+
+            ContentSafetySampleAnalyzeImage sampleAnalyzeImage = new ContentSafetySampleAnalyzeImage();
+            sampleAnalyzeImage.AnalyzeImage();
+
+            ContentSafetySampleManageBlocklist sampleManageBlocklist = new ContentSafetySampleManageBlocklist();
+            sampleManageBlocklist.ManageBlocklist();
+        }
+    }
+}
